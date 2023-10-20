039ff272fd037d45d9dbdc5fe26476d099e95a1c
C. Ward <celina@ngrok.com>
2023-10-20 10:58:42 -0700

    update to version 3.3.5 (#10)
    Co-authored-by: ngrok-release-bot <release-noreply@ngrok.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,14 +3,14 @@ $packageArgs = @{
   packageName   = 'ngrok'
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://bin.equinox.io/a/8n212tLRmgF/ngrok-v3-3.3.2-windows-386.zip'
-  url64bit      = 'https://bin.equinox.io/a/2oeWj5GFKLq/ngrok-v3-3.3.2-windows-amd64.zip'
+  url           = 'https://bin.equinox.io/a/5BvjhrDzf9Q/ngrok-v3-3.3.5-windows-386.zip'
+  url64bit      = 'https://bin.equinox.io/a/45wEnzgJxa9/ngrok-v3-3.3.5-windows-amd64.zip'
 
   softwareName  = 'ngrok'
 
-  checksum      = 'bee7e102549d0a7f1ee577fee2395b821e5db31f428da76203029f79dc0ab18a'
+  checksum      = 'cdd6b31a9f88c1c4fedcf90ace2e6379bf1825a9f47e7f53e3b0283d660df3af'
   checksumType  = 'sha256'
-  checksum64    = '17e6b5710c2ff2759a97b0830622456657c4b2087d07cc7ad32f94135a612c59'
+  checksum64    = 'df7f8d667a2842cad20da9dea1e8eebae6a970fea83dd3ce8cd5357e0b64a856'
   checksumType64= 'sha256'
 
   validExitCodes= @(0)
